10a5f708492c90514545e61afdffb6d90bb29930
dlsundn <sw020406@naver.com>
2025-08-27 15:32:21 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 90.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        int result = 0;
+        
+        for(int i = 0; i < number.length-2; i++){
+            for(int j = i+1; j < number.length-1; j++){
+              for(int k = j+1; k < number.length; k++){
+                      answer = number[i] + number[j] + number[k];
+                      if(answer == 0){
+                          result ++;
+                      }
+                  }
+
+        }
+                }
+             
+            
+            
+        
+        
+        return result;
+    }
+}
